11c8d7c0edcc28d57edd907d68efe4cda4ca3e48
jerry8534 <jerryjjinu@gmail.com>
2023-01-10 13:46:31 +0900

    order

--- TravelMaker/src/main/java/com/project1/order/OrderController.java
+++ TravelMaker/src/main/java/com/project1/order/OrderController.java
@@ -1,11 +1,13 @@
 package com.project1.order;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
+@Slf4j
 public class OrderController {
 
     @Autowired
@@ -14,8 +16,8 @@ public class OrderController {
     @RequestMapping("/order/regionSelect")
     public ModelAndView regionSelect() {
         ModelAndView mv = new ModelAndView();
+        log.info("regionSelect");
         mv.setViewName("order/regionSelect");
         return mv;
     }
-
 }
