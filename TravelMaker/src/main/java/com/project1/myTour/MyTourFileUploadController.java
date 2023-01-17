872231aa46b0e09430e496f52d1b6cf85027fbf4
hyeon605 <happilyah@naver.com>
2023-01-17 14:40:32 +0900

    pull할겨

--- TravelMaker/src/main/java/com/project1/myTour/MyTourFileUploadController.java
+++ TravelMaker/src/main/java/com/project1/myTour/MyTourFileUploadController.java
@@ -32,7 +32,9 @@ import com.google.gson.JsonObject;
 public class MyTourFileUploadController {
 	static String uploadPath="C:\\Users\\junah\\git\\TravelMaker\\TravelMaker\\src\\main\\resources\\static\\upload\\";
 	UUID uuid = null;
+
 	String tempDir = "C:\\Temp\\";
+
 	HttpServletRequest req;
 	
 	@RequestMapping(value="/MyTour/MyTourReviewInsert")
