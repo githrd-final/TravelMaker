1e8205eca1cf99dde3e41f551ef663599c537c71
jerry8534 <jerryjjinu@gmail.com>
2023-01-25 13:38:25 +0900

    가격 , 추가

--- TravelMaker/src/main/resources/static/js/purchaseCheck.js
+++ TravelMaker/src/main/resources/static/js/purchaseCheck.js
@@ -5,7 +5,30 @@
 var frm = $("#frm");
 var email = document.getElementById("email").value;
 var region = document.getElementById("region").value;
+var regionString = "";
+if(region == "1") {
+    regionString = "강원도";
+}else if(region == "3") {
+    regionString = "경상도";
+}else if(region == "4") {
+    regionString = "전라도";
+}else if(region == "5") {
+    regionString = "충청도";
+}else if(region == "7") {
+    regionString = "전국";
+}
 var people = document.getElementById("people").value;
+var price = 0;
+var totalPrice = 0;
+if(region == "7") {
+    price = 30000;
+}else if(region == "3" || region == "4") {
+    price = 40000;
+}else if(region == "5" || region == "1") {
+    price = 35000;
+}
+
+totalPrice = people* price;
 const rand = Math.floor(Math.random() * 2000);
 function requestPay() {
 var IMP = window.IMP; // 생략 가능
@@ -16,8 +39,8 @@ IMP.init("imp84346376"); // 예: imp00000000
     IMP.request_pay({ // param
         pg: "html5_inicis",
         pay_method: "card",
-        merchant_uid: "ORD20230125-" + rand,
-        name: "랜덤 기차 여행 - " + region,
+        merchant_uid: "ORD20230125123-" + rand,
+        name: "랜덤 기차 여행 - " + regionString,
         amount: 100, // if
         buyer_email: email,
         buyer_name: "양수비",
